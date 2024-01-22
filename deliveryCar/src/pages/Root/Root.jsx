d7ffe96e48c2c2469ad8a9076be4bacbdc667def
Pedro Souza <pedrosport10@gmail.com>
2024-01-22 15:20:06 -0300

    FEAT: Criando componente do footer e ajustando para o outlet

--- deliveryCar/src/pages/Root/Root.jsx
+++ deliveryCar/src/pages/Root/Root.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Root = () => {
   return (
     <>
+      <Header />
+      
+      <main>
         <Outlet />
-    </>
-  )
-}
+      </main>
 
-export default Root
+      <Footer />
+    </>
+  );
+};
+
+export default Root;
