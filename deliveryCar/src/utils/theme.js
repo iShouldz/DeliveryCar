bdde0cab4e5ed4c65e8b9a3a70823a51060edcd9
Pedro Souza <pedrosport10@gmail.com>
2024-01-23 17:15:10 -0300

    FEAT: Criando theme

---
+++ deliveryCar/src/utils/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#282828',
+            light: '#FFF',
+            dark: '#FFFFFF'
+        },
+        secondary: {
+            main: '#FBA403'
+        }
+    }
+})
