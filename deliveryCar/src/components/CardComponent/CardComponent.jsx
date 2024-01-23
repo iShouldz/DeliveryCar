aa9137cc9d85fda6847cfda8592f2631031d7bb7
Pedro Souza <pedrosport10@gmail.com>
2024-01-23 20:14:45 -0300

    FEAT: Estilizando o card component

--- deliveryCar/src/components/CardComponent/CardComponent.jsx
+++ deliveryCar/src/components/CardComponent/CardComponent.jsx
@@ -7,20 +7,24 @@ import {
   Typography,
 } from "@mui/material";
 import Card from "@mui/material/Card";
+import styles from "./styles.module.css";
 
 const CardComponent = ({ img, alt, title, description }) => {
   return (
-    <Card sx={{ maxWidth: "331px" }}>
+    <Card
+      sx={{ maxWidth: "331px", backgroundColor: "primary.grayRef", gap: "6px" }}
+      className={styles.cardContainer}
+    >
       <CardMedia component="img" alt={alt} image={img} />
 
       <CardContent>
         <Typography>{title}</Typography>
 
-        <Typography>{description}</Typography>
+        <Typography sx={{ color: "secondary.light" }}>{description}</Typography>
       </CardContent>
 
       <CardActions sx={{ justifyContent: "flex-end" }}>
-        <Button>Learn more</Button>
+        <Button sx={{ color: "secondary.main" }}>Learn more</Button>
       </CardActions>
     </Card>
   );
