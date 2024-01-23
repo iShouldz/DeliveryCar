4381d974b31a2e4e424054e23b5277fcf80155f8
Pedro Souza <pedrosport10@gmail.com>
2024-01-22 23:24:06 -0300

    FEAT: Ajustando header funcional

--- deliveryCar/src/components/Header/Header.jsx
+++ deliveryCar/src/components/Header/Header.jsx
@@ -1,16 +1,59 @@
+import { AppBar, Box, IconButton, Tab, Tabs, Toolbar } from "@mui/material";
+import logo from "../../assets/Logo.svg";
+import userLogo from '../../assets/user-avatar.svg'
+import notification from '../../assets/bell-icon.svg'
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleGoTo = (url) => {
+    navigate(`/${url}`);
+  };
+
   return (
     <header>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Getting a Taxi</li>
-          <li>Mobile App</li>
-          <li>Contact Us</li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <IconButton
+            size="large"
+            aria-controls="headerMenu"
+            aria-haspopup="true"
+          >
+            <img src={logo} alt="Logo da empresa" />
+          </IconButton>
 
-export default Header
+          <Tabs id="headerMenu" sx={{ display: "flex" }}>
+            <Tab label="Home" onClick={() => handleGoTo("")} />
+
+            <Tab label="Getting a Taxi" />
+
+            <Tab label="Mobile App" onClick={() => handleGoTo("mobile-app")} />
+
+            <Tab label="Contact Us" onClick={() => handleGoTo("contact-us")} />
+          </Tabs>
+
+          <Box>
+            <IconButton
+              size="large"
+              aria-controls="headerMenu"
+              aria-haspopup="true"
+            >
+              <img src={notification} alt="Logo da empresa" />
+            </IconButton>
+
+            <IconButton
+              size="large"
+              aria-controls="headerMenu"
+              aria-haspopup="true"
+            >
+              <img src={userLogo} alt="Logo da empresa" />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+};
+
+export default Header;
