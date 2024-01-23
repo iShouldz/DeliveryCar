70e3b2bf66218c09ad0b133b410c928e62431984
Pedro Souza <pedrosport10@gmail.com>
2024-01-23 10:58:26 -0300

    FEAT: Estruturando conteudo do form

--- deliveryCar/src/pages/Home/EndHome/EndHome.jsx
+++ deliveryCar/src/pages/Home/EndHome/EndHome.jsx
@@ -1,27 +1,28 @@
 /* eslint-disable no-unused-vars */
-import * as yup from "yup";
-//import ErrosForm from "../../components/ErrosForm/ErrosForm";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { Button, TextField } from "@mui/material";
 import FormCar from "../../../components/FormCar/FormCar";
-
-const schema = yup
-  .object({
-    fullName: yup.string().required(),
-    emailUser: yup.string().required(),
-    placaUser: yup.string().required(),
-  })
-  .required();
-
+import styles from "./styles.module.css";
+import carForm from "../../../assets/form/CarFormImage.svg";
 const EndHome = () => {
   return (
-    <>
+    <section className={styles.containerMain}>
+      <div className={styles.formContainer}>
+        <section className={styles.headerForm}>
+          <img src={carForm} />
+
+          <article id={styles.headerContent}>
+            <h2>Drive with MyRide</h2>
+            <p>
+              Register as a driver using the form below. Our team will assess
+              and get back to you within 48 hours.
+            </p>
+          </article>
+        </section>
+
         <h1>Form</h1>
         <FormCar />
-    </>
-  )
+      </div>
+    </section>
+  );
 };
 
 export default EndHome;
