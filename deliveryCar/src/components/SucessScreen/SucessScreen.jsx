509ec03b10e09bb7bf206e0b336f672a983dd8df
Pedro Souza <pedrosport10@gmail.com>
2024-01-24 16:23:17 -0300

    FIX: Ajustando sucess screen

--- deliveryCar/src/components/SucessScreen/SucessScreen.jsx
+++ deliveryCar/src/components/SucessScreen/SucessScreen.jsx
@@ -5,13 +5,10 @@ import ButtonCar from "../UI/ButtonCar/ButtonCar";
 import check from "../../assets/check.png";
 import car from "../../assets/form/CarFormImage.svg";
 import TextFieldDisplay from "../UI/TextFieldDisplay/TextFieldDisplay";
-import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { carActions } from "../../store/cars/carSlice";
 
 const SucessScreen = () => {
-  const [dataGet, setDataGet] = useState([]);
-  const fetchStatus = useSelector((state) => state.cars.fetching);
   const fetchItem = useSelector((state) => state.cars.item);
   const lastElement = fetchItem[fetchItem.length - 1];
   const dispatch = useDispatch();
@@ -46,28 +43,27 @@ const SucessScreen = () => {
     <section className={styles.sucessContainer}>
       <article id={styles.title}>
         <img src={check} />
-        <Typography variant="h3" sx={{ color: "primary.light" }}>
+        <Typography variant="h3" sx={{ color: "primary.light", marginBottom: '20px'}}>
           Welcome, {lastElement.fullName}
         </Typography>
       </article>
 
-      <Typography>That's data your send to us! </Typography>
+      <Typography sx={{marginBottom: '20px'}}>That's data your send to us! </Typography>
 
       <section className={styles.userInfoContainer}>
         <TextFieldDisplay value={lastElement.fullName} label="Full Name" />
         <TextFieldDisplay value={lastElement.country} label="Country" />
         <TextFieldDisplay value={lastElement.city} label="City" />
         <TextFieldDisplay value={lastElement.placaUser} label="Placa" />
-        <TextFieldDisplay value={lastElement.fullName} />
-
+        
         <Typography>
           You'll so many info about process in your email:{" "}
           <span style={{ color: "orange" }}>{lastElement.emailUser}</span>
         </Typography>
       </section>
 
-      <img src={car} />
-      <ButtonCar color="primary.light" onClick={handleSubmitNewCar}>
+      <img src={car} id={styles.img} />
+      <ButtonCar color="primary.light" onClick={handleSubmitNewCar} sx={{marginBottom: '20px'}}>
         Submit a new car{" "}
       </ButtonCar>
     </section>
