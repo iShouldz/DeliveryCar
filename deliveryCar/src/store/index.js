3b5499af372b33c0fd4c9119684f50009e4be4c0
Pedro Souza <pedrosport10@gmail.com>
2024-01-25 23:52:46 -0300

    FEAT: Definindo o redux para o login

--- deliveryCar/src/store/index.js
+++ deliveryCar/src/store/index.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import carSlice from "./cars/carSlice";
-
+import loginSlice from "./login/loginSlice";
 const store = configureStore({
-    reducer: { cars: carSlice.reducer },
+    reducer: { cars: carSlice.reducer, login: loginSlice.reducer },
 });
 
 export default store;
