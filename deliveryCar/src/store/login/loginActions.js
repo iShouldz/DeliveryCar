b03cfc416d03c2a18ef14831fe5311686ca00a58
Pedro Souza <pedrosport10@gmail.com>
2024-01-27 17:25:12 -0300

    FEAT: Movendo delete pro actions

--- deliveryCar/src/store/login/loginActions.js
+++ deliveryCar/src/store/login/loginActions.js
@@ -13,4 +13,23 @@ async function getDataCars() {
     return carData
 }
 
-export { getDataCars }
+const deleteCar = async (carId) => {
+    try {
+      const response = await fetch(`http://localhost:3000/cars/${carId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        console.log(`Car with id ${carId} deleted successfully`);
+      } else {
+        console.error(`Failed to delete car with id ${carId}`);
+      }
+    } catch (error) {
+      console.error("Error deleting car:", error);
+    }
+  };
+
+export { getDataCars, deleteCar }
