d932ebb071bd8a94fd753498332b77695eb44212
Pedro Souza <pedrosport10@gmail.com>
2024-01-27 17:02:36 -0300

    FEAT: Estruturando dashboard

--- deliveryCar/src/pages/Dashboard/Dashboard.jsx
+++ deliveryCar/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDataCars } from "../../store/login/loginActions";
+import { carActions } from "../../store/cars/carSlice";
+import CardDriver from "../../components/CardDriver/CardDriver";
+import { Typography } from "@mui/material";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const dispatch = useDispatch();
+  const [isFetching, setIsFetching] = useState(false);
+  const carData = useSelector((state) => state.cars.item);
 
-export default Dashboard
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDataCars();
+      dispatch(carActions.handleCarItem(data));
+    };
+    fetchData();
+    setIsFetching(true);
+  }, [dispatch]);
+
+  return (
+    <>
+      <Typography
+        variant="h2"
+        color="secondary.main"
+        sx={{
+          fontWeight: "bold",
+          display: "flex",
+          justifyContent: "center",
+          padding: "30px",
+        }}
+      >
+        DashBoard
+      </Typography>
+      <section
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "20px",
+        }}
+      >
+        {isFetching &&
+          carData.map((item) => (
+            <CardDriver
+              key={Math.random()}
+              name={item.fullName}
+              city={item.city}
+              country={item.country}
+              email={item.emailUser}
+              myCar={item.myCar !== '' ? item.selectedCar : ''}
+            />
+          ))}
+      </section>
+    </>
+  );
+};
+
+export default Dashboard;
